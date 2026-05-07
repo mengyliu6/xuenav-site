3b4ed3f8c9006b9767ebb76e24755260620d0f10
HeT <996465962@qq.com>
2026-05-07 11:48:39 +0800

    添加图片

--- src/data/products.js
+++ src/data/products.js
@@ -1,174 +1,194 @@
 import camaroRadio from "../assets/images/product/chevrolet-camaro-radio-2010-2015.jpg.jpeg";
 import camaroCluster from "../assets/images/product/chevrolet-camaro-ss-cluster-2010-2015.jpg";
 import VogueRadio from "../assets/images/product/range-rover-l494-l405-radio-2013-2017.jpg";
-import VogueACPanel from "../assets/images/product/range-rover-l405-climate-control.jpg";
+import VogueACPanel from "../assets/images/product/range-rover-l494-l405-radio-2013-2017.jpg";
+import SportACPanel from "../assets/images/product/image (5).jpeg";
+import XFRadio from "../assets/images/product/6.jpeg";
+import DurangoRadio from "../assets/images/product/Durango.png";
+import GMCRadio from "../assets/images/product/GMC.png";
+import GMCCluster from "../assets/images/product/image (6).jpeg";
+import GMCRadio1923 from "../assets/images/product/836.png";
+import White834Pro from "../assets/images/product/image (7).jpeg";
+import Black834Pro from "../assets/images/product/image (8).jpeg";
+import R8Radio from "../assets/images/product/image (9).jpeg";
+import GhibliRadio from "../assets/images/product/image (10).jpeg";
+import GTRadio from "../assets/images/product/image (11).jpeg";
+import GTCluster from "../assets/images/product/image (12).jpeg";
+import GTRRadio from "../assets/images/product/image (13).jpeg";
+import GTRCluster from "../assets/images/product/GTR.png";
+import GTRadio0210 from "../assets/images/product/image (14).jpeg";
+import JaguarAC from "../assets/images/product/image (15).jpeg";
+import XJAC from "../assets/images/product/image (16).jpeg";
+import F10AC from "../assets/images/product/image (17).jpeg";
+import F10Cluster from "../assets/images/product/image (18).jpeg";
+import X3Cluster from "../assets/images/product/image (19).jpeg";
 
 export const products = [
   {
-    id: "camaro-radio",
+    id: "camaro-radio-10-15",
     name: "Car Radio for Chevrolet Camaro 2010-2015",
     image: camaroRadio,
     videoUrl: "https://www.youtube.com/watch?v=eOQ_AGZgMEM&t=4s",
     start: 0,
   },
   {
-    id: "camaro-cluster",
+    id: "camaro-cluster-10-15",
     name: "Digital Cluster for Chevrolet Camaro SS 2010-2015",
     image: camaroCluster,
     videoUrl: "https://www.youtube.com/watch?v=gbw0AUKcKzY",
     start: 0,
   },
   {
-    id: "range-rover-radio",
+    id: "range-rover-radio-L494-L405",
     name: "Car Radio For Range Rover Sport L494 Vogue L405",
     image: VogueRadio,
     videoUrl: "https://www.youtube.com/watch?v=14E1m4wayKI&t=690s",
     start: 690,
   },
   {
-    id: "range-rover-ac",
+    id: "range-rover-ac-L405",
     name: "Climate Control For Range Rover Vogue L405",
     image: VogueACPanel,
     videoUrl: "https://www.youtube.com/watch?v=livNmFk9vxY",
     start: 0,
   },
   {
-    id: "5",
+    id: "range-rover-ac-L494",
     name: "Climate Control For Range Rover Sport L494",
-    image: "",
+    image: SportACPanel,
     videoUrl: "https://www.youtube.com/watch?v=Dry1hMnXqA0",
     start: 0,
   },
   {
-    id: "6",
+    id: "jaguar-xf-radio-08-15",
     name: "Car Radio for Jaguar XF 2008-2015",
-    image: "",
+    image: XFRadio,
     videoUrl: "https://www.youtube.com/watch?v=v5MBkw8sRcs&pp=0gcJCdQKAYcqIYzv",
     start: 0,
   },
   {
-    id: "7",
+    id: "dodge-durango-radio-11-13",
     name: "Car Radio for Dodge Durango 2011 2012 2013",
-    image: "",
+    image: DurangoRadio,
     videoUrl: "https://www.youtube.com/watch?v=dFH7lySij4M&t=48s",
     start: 0,
   },
   {
-    id: "8",
+    id: "gmc-radio-07-13",
     name: "Car Radio for Chevrolet Silverado GMC 2007-2013",
-    image: "",
+    image: GMCRadio,
     videoUrl: "",
     start: 0,
   },
   {
-    id: "9",
+    id: "gmc-cluster-07-13",
     name: "Digital Cluster for Chevrolet Silverado GMC 2007-2013",
-    image: "",
+    image: GMCCluster,
     videoUrl: "https://www.youtube.com/watch?v=UTMfIX-hsac&pp=0gcJCdQKAYcqIYzv",
     start: 0,
   },
   {
-    id: "10",
+    id: "gmc-radio-19-23",
     name: "Car Radio for Chevrolet Silverado GMC Sierra 2019-2023",
-    image: "",
+    image: GMCRadio1923,
     videoUrl: "https://www.youtube.com/watch?v=Ie2TcqUMv34&t=8s",
     start: 0,
   },
   {
-    id: "11",
+    id: "834pro-white",
     name: "Car Radio for Chevrolet Silverado GMC Sierra 2014-2018",
-    image: "",
+    image: White834Pro,
     videoUrl: "https://youtu.be/7irx9v6bYFg",
     start: 0,
   },
   {
-    id: "12",
+    id: "834pro-black",
     name: "Car Radio for Chevrolet Silverado GMC Sierra 2014-2018",
-    image: "",
+    image: Black834Pro,
     videoUrl: "https://www.youtube.com/watch?v=-PII_8MpO5g",
     start: 0,
   },
   {
-    id: "13",
+    id: "audi-r8-radio-07-15",
     name: "Car Radio for Audi R8 2007-2015",
-    image: "",
+    image: R8Radio,
     videoUrl: "https://www.youtube.com/watch?v=9kqm7fGJ4PQ",
     start: 0,
   },
   {
-    id: "14",
+    id: "ghibli-radio-14-19",
     name: "Car Radio for Maserati Ghibli 2014-2019",
-    image: "",
+    image: GhibliRadio,
     videoUrl: "https://www.youtube.com/watch?v=rtkadZK0-pA",
     start: 0,
   },
   {
-    id: "15",
+    id: "gt-radio-07-17",
     name: "Android Radio for Maserati GT 2007-2017",
-    image: "",
+    image: GTRadio,
     videoUrl: "https://www.youtube.com/watch?v=jEY56qyg-Eo&t=27s",
     start: 0,
   },
   {
-    id: "16",
+    id: "gt-cluster-07-17",
     name: "Digital Cluster for Maserati GT/GC GranTurismo 2007-2017",
-    image: "",
+    image: GTCluster,
     videoUrl: "https://www.youtube.com/watch?v=cCyONWATDBo",
     start: 0,
   },
   {
-    id: "17",
+    id: "gtr-radio-08-15",
     name: "Radio for Nissan GTR R35 2008-2015",
-    image: "",
+    image: GTRRadio,
     videoUrl: "https://youtu.be/RYFNeTlbxlA",
     start: 0,
   },
   {
-    id: "18",
+    id: "gtr-cluster-07-16",
     name: "Digital Cluster Gauge for Nissan GTR GT-R R35 2007-2016",
-    image: "",
+    image: GTRCluster,
     videoUrl: "https://www.youtube.com/watch?v=a1AGA0qQiys",
     start: 0,
   },
   {
-    id: "19",
+    id: "gt-radio-02-10",
     name: "Radio for Maserati 3200 GT4200 GT Coupe 2002-2010 ",
-    image: "",
+    image: GTRadio0210,
     videoUrl: "https://youtu.be/LMfxnrvnzpY",
     start: 0,
   },
   {
-    id: "20",
+    id: "jaguar-ac-15-19",
     name: "Climate Control for 2015-2019 Jaguar XE XEL XF XFL F-PACE",
-    image: "",
+    image: JaguarAC,
     videoUrl: "",
     start: 0,
   },
   {
-    id: "21",
+    id: "xj-ac",
     name: "car Climate Control  for 2009-2018 Jaguar XJ XJL",
-    image: "",
+    image: XJAC,
     videoUrl: "",
     start: 0,
   },
   {
-    id: "22",
+    id: "bmw-f10-ac-11-17",
     name: "",
-    image: "",
+    image: F10AC,
     videoUrl: "Climate Control for BMW F10 F11 5 Series 2011-2017",
     start: 0,
   },
   {
-    id: "23",
+    id: "bmw-f10-cluster-11-17",
     name: "for BMW 5 Series F10 F11 F18 2011-2017 Digital Speedometer with HUD",
-    image: "",
+    image: F10Cluster,
     videoUrl: "https://www.youtube.com/watch?v=p5sJeTuYptU",
     start: 0,
   },
   {
-    id: "23",
+    id: "bmw-x3-cluster",
     name: "Digital Instrument for BMW 5 6 7Series X3 X4 X5 X6",
-    image: "",
+    image: X3Cluster,
     videoUrl: "https://www.youtube.com/watch?v=Jrquk4KBGE0&t=9s",
     start: 0,
   },
