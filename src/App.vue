c0d4172ff087329639a95310bcedbe78652e2706
HeT <996465962@qq.com>
2026-05-18 14:35:51 +0800

    新增Whatsapp悬浮按钮

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <RouterView />
+  <WhatsAppFloat />
   <BackToTop />
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
 import BackToTop from "./components/BackToTop.vue";
+import WhatsAppFloat from "./components/WhatsAppFloat.vue";
 </script>
